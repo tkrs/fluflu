a5b226a222d4d97ae8b4ba8af794929a7c7748d8
Takeru Sato <type.in.type@gmail.com>
2017-04-17 03:44:38 +0900

    🔧 Use `stream()` instead of `parallelStream()`

--- queue/src/main/scala/fluflu/queue/Async.scala
+++ queue/src/main/scala/fluflu/queue/Async.scala
@@ -19,11 +19,12 @@ final case class Async(
 )(implicit clock: Clock) extends LazyLogging {
 
   private[this] val letterQueue: BlockingDeque[() => Throwable \/ Letter] = new LinkedBlockingDeque()
-  private[this] val scheduler = Executors.newScheduledThreadPool(1)
+  private[this] val scheduler = Executors.newSingleThreadScheduledExecutor()
+
   private[this] val consume: (() => Throwable \/ Letter) => Unit = { fn =>
     letterQueue.remove(fn)
     fn()
-      .leftMap(logger.error(s"Failed to encode a message to Message-Pack", _))
+      .leftMap(logger.error(s"Failed to encode a message to MessagePack", _))
       .flatMap(messenger.write).fold(
         e => logger.error(s"Failed to send a message to remote: ${messenger.host}:${messenger.port}", e),
         _ => ()
@@ -31,14 +32,17 @@ final case class Async(
   }
 
   private[this] val command: Runnable = new Runnable {
-    override def run(): Unit =
-      letterQueue.parallelStream().forEach(asJavaConsumer(consume))
+    override def run(): Unit = {
+      val start = if (!letterQueue.isEmpty) System.nanoTime() else 0
+      letterQueue.stream.forEach(asJavaConsumer(consume))
+      if (start > 0) logger.debug(s"A command spend ${TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start)} ms")
+    }
   }
 
   private[this] val _: ScheduledFuture[_] =
     scheduler.scheduleWithFixedDelay(command, initialDelay.toNanos, delay.toNanos, TimeUnit.NANOSECONDS)
 
-  def size: Int = letterQueue.size
+  def remaining: Int = letterQueue.size
 
   def push[A: Encoder](e: Event[A]): Exception \/ Unit =
     if (letterQueue offer (() => Messages.pack(e).map(Letter))) \/.right(())
@@ -48,7 +52,7 @@ final case class Async(
     scheduler.shutdown()
     scheduler.awaitTermination(terminationDelay.toNanos, TimeUnit.NANOSECONDS)
     if (!scheduler.isTerminated) scheduler.shutdownNow()
-    if (!letterQueue.isEmpty) logger.debug(s"message queue has remaining: ${letterQueue.size()}")
+    if (!letterQueue.isEmpty) logger.debug(s"A message queue has remaining: ${letterQueue.size()}")
     command.run()
     messenger.close()
   }
