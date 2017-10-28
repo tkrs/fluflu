1c9693389ab84e95c3916471c7eb74f53e2e088a
Takeru Sato <type.in.type@gmail.com>
2017-10-28 23:18:04 +0900

    Update scalafmt version to 1.2.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,6 +4,7 @@ object Deps {
   val Ver = new {
     val `scala2.12`      = "2.12.4"
     val `scala2.11`      = "2.11.11"
+    val scalafmt         = "1.2.0"
     val cats             = "1.0.0-MF"
     val circe            = "0.9.0-M1"
     val scalacheck       = "1.13.5"
