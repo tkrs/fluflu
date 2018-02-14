f792972795c549463f379ea27e1b492de809f97a
Takeru Sato <type.in.type@gmail.com>
2018-02-15 06:18:11 +0900

    Replace with Json.Folder

--- modules/msgpack-circe/src/main/scala/fluflu/msgpack/circe/MessagePacker.scala
+++ modules/msgpack-circe/src/main/scala/fluflu/msgpack/circe/MessagePacker.scala
@@ -1,10 +1,11 @@
 package fluflu.msgpack
 package circe
 
-import io.circe.{Encoder, Json}
+import io.circe.{Encoder, Json, JsonNumber, JsonObject}
 import io.circe.syntax._
+import org.msgpack.core.{MessagePacker => CMessagePacker}
+import org.msgpack.core.MessagePack
 import org.msgpack.core.MessagePack.PackerConfig
-import org.msgpack.core.{MessageBufferPacker, MessagePack}
 
 import scala.util.{Failure, Success, Try}
 
@@ -15,46 +16,49 @@ object MessagePacker {
 
 final class MessagePacker(config: PackerConfig) {
 
-  def encode[A: Encoder](a: A): Either[Throwable, Array[Byte]] = pack(a.asJson)
+  def encode[A: Encoder](a: A): Either[Throwable, Array[Byte]] =
+    pack(a.asJson)
 
-  def pack(doc: Json): Either[Throwable, Array[Byte]] = {
-    Packer.using(Try(config.newBufferPacker()))(r => Try { go(doc, r); r.toByteArray }) match {
+  def pack(doc: Json): Either[Throwable, Array[Byte]] =
+    Packer.using(Try(config.newBufferPacker()))(r => Try { val _ = go(doc, r); r.toByteArray }) match {
       case Success(v) => Right(v)
       case Failure(e) => Left(e)
     }
-  }
 
   private def double(x: BigDecimal): Boolean = x.scale != 0
 
-  private def go(json: Json, acc: MessageBufferPacker): Unit =
-    json.fold[Unit](
-      acc.packNil(),
-      acc.packBoolean,
-      x => {
-        val n = x.toBigDecimal
-        n match {
-          case None => ()
-          case Some(v) if double(v) =>
-            acc.packDouble(v.toDouble)
-          case Some(v) if v.isValidLong =>
-            acc.packLong(v.toLong)
-          case Some(v) =>
-            acc.packBigInteger(v.toBigInt().bigInteger)
+  private def go(json: Json, acc: CMessagePacker): CMessagePacker = {
+    json.foldWith(
+      new Json.Folder[CMessagePacker] with ((CMessagePacker, (String, Json)) => CMessagePacker) {
+        def apply(acc: CMessagePacker, kv: (String, Json)): CMessagePacker = kv match {
+          case (k, v) =>
+            acc.packString(k)
+            v.foldWith(this)
+            acc
         }
-      },
-      acc.packString,
-      xs => {
-        acc.packArrayHeader(xs.size)
-        xs.foreach(go(_, acc))
-      },
-      x => {
-        val xs = x.toList
-        acc.packMapHeader(xs.size)
-        xs.foreach {
-          case (key, (v)) =>
-            acc.packString(key)
-            go(v, acc)
+        def onNull: CMessagePacker =
+          acc.packNil()
+        def onBoolean(value: Boolean): CMessagePacker =
+          acc.packBoolean(value)
+        def onNumber(value: JsonNumber): CMessagePacker =
+          value.toBigDecimal match {
+            case None =>
+              acc
+            case Some(v) if double(v) =>
+              acc.packDouble(v.toDouble)
+            case Some(v) if v.isValidLong =>
+              acc.packLong(v.toLong)
+            case Some(v) =>
+              acc.packBigInteger(v.toBigInt().bigInteger)
+          }
+        def onString(value: String): CMessagePacker =
+          acc.packString(value)
+        def onArray(value: Vector[Json]): CMessagePacker =
+          value.foldLeft(acc.packArrayHeader(value.size))((_, v) => v.foldWith(this))
+        def onObject(value: JsonObject): CMessagePacker = {
+          val xs = value.toVector
+          xs.foldLeft(acc.packMapHeader(xs.size))(this)
         }
-      }
-    )
+      })
+  }
 }
