ec79f9559a097cad1cc9c0b9456f8c22f68fcb7a
Takeru Sato <type.in.type@gmail.com>
2018-08-30 02:24:12 +0900

    Update README, Optimize benchmark settings

--- modules/benchmark/src/test/scala/fluflu/msgpack/Bench.scala
+++ modules/benchmark/src/test/scala/fluflu/msgpack/Bench.scala
@@ -9,7 +9,21 @@ import org.openjdk.jmh.annotations._
 @Warmup(iterations = 10, time = 1)
 @Measurement(iterations = 10, time = 1)
 @OutputTimeUnit(TimeUnit.SECONDS)
-@Fork(2)
+@Fork(
+  value = 2,
+  jvmArgs = Array(
+    "-server",
+    "-Xms2g",
+    "-Xmx2g",
+    "-XX:NewSize=1g",
+    "-XX:MaxNewSize=1g",
+    "-XX:InitialCodeCacheSize=512m",
+    "-XX:ReservedCodeCacheSize=512m",
+    "-XX:+UseParallelGC",
+    "-XX:-UseBiasedLocking",
+    "-XX:+AlwaysPreTouch"
+  )
+)
 abstract class Bench
 
 class PackBench   extends Bench with circe.PackerBench
