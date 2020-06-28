b4f6e047c120004ab0b188bf5cd2691131fac1af
Takeru Sato <type.in.type@gmail.com>
2020-06-29 00:06:49 +0900

    build: update dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,17 +3,17 @@ import sbt._
 object Deps {
   val Ver = new {
     val `scala2.12` = "2.12.11"
-    val `scala2.13` = "2.13.2"
+    val `scala2.13` = "2.13.3"
 
     val msgpackJava  = "0.8.20"
     val mess         = "0.2.1"
     val scalaLogging = "3.9.2"
     val logback      = "1.2.3"
 
-    val scalatest = "3.1.1"
+    val scalatest = "3.2.0"
     val scalatestplus = new {
-      val scalacheck = "3.1.1.1"
-      val mockito    = "3.1.1.0"
+      val scalacheck = "3.2.0.0"
+      val mockito    = "3.2.0.0"
     }
   }
 
@@ -30,7 +30,7 @@ object Deps {
 
     lazy val scalatest  = "org.scalatest"     %% "scalatest"       % Ver.scalatest
     lazy val scalacheck = "org.scalatestplus" %% "scalacheck-1-14" % Ver.scalatestplus.scalacheck
-    lazy val mockito    = "org.scalatestplus" %% "mockito-3-2"     % Ver.scalatestplus.mockito
+    lazy val mockito    = "org.scalatestplus" %% "mockito-3-3"     % Ver.scalatestplus.mockito
 
     lazy val forTest = Seq(scalatest, scalacheck, mockito).map(_ % Test)
   }
