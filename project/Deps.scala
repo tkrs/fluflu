c12a841d92311d419d931459a2fb9bb13481d698
Takeru Sato <type.in.type@gmail.com>
2020-09-08 01:59:19 +0900

    build: update to mess 0.2.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
     val organizeImports = "0.4.0"
 
     val msgpackJava  = "0.8.20"
-    val mess         = "0.2.1"
+    val mess         = "0.2.2"
     val scalaLogging = "3.9.2"
     val logback      = "1.2.3"
 
