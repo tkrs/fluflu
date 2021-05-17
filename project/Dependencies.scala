370d75218522c2138fc61d0e537efc0f07be0373
Scala Steward <me@scala-steward.org>
2021-05-17 14:54:53 +0200

    build(deps): bump scala-library from 2.13.5 to 2.13.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val Ver = new {
     val `scala2.12` = "2.12.12"
-    val `scala2.13` = "2.13.5"
+    val `scala2.13` = "2.13.6"
 
     val organizeImports = "0.5.0"
 
