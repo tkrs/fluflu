d97c5e05472d5f972ec0e9e977b2452e42d16e76
Scala Steward <scala_steward@virtuslab.com>
2023-06-11 00:34:57 +0000

    build(deps): bump scala-library from 2.12.17 to 2.12.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val Ver = new {
-    val `scala2.12` = "2.12.17"
+    val `scala2.12` = "2.12.18"
     val `scala2.13` = "2.13.10"
     val scala3      = "3.3.0"
 
